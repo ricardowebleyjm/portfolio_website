4fb1e6f00ef0bdc79bc02930f33d1c8d99408968
Ricardo <webleyricardo@yahoo.com>
2025-01-16 23:40:37 -0500

    added trusted domains

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.CustomUser"
+
+CSRF_TRUSTED_ORIGINS = config.get('secrets', "CSRF_TRUSTED_ORIGINS").split(',')
+
+print(CSRF_TRUSTED_ORIGINS)
